95bdf790bbe404234ebe34bb1de750919a402d0c
David Shapiro <daveshap37@gmail.com>
2022-09-09 13:53:53 -0400

    done for now

--- simulation.py
+++ simulation.py
@@ -18,7 +18,7 @@ def save_file(filepath, content):
 
 
 openai.api_key = open_file('openaiapikey.txt')
-rarities = ['common', 'likely', 'unlikely', 'interesting', 'exciting', 'funny', 'stressful', 'irritating', 'ordinary', 'extraordinary', 'shocking']
+rarities = ['common', 'likely', 'unlikely', 'interesting', 'exciting', 'funny', 'stressful', 'irritating', 'ordinary'] #, 'extraordinary'] #, 'shocking']  # shocking might cause t he story to end
 
 
 def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.0, stop=['asdfasdf', 'asdasdf']):
